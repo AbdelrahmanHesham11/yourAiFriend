020624bee69d5e590d0bd10417fcc0debb4a6786
Abdelrahman Hesham <abdo.heshamfathala@gmail.com>
2026-01-24 21:40:37 +0200

    added error handling

--- src/lib/vapi.ts
+++ src/lib/vapi.ts
@@ -1,7 +1,7 @@
 import Vapi from "@vapi-ai/web";
 
 if (!process.env.NEXT_PUBLIC_VAPI_API_KEY) {
-  console.warn("⚠️ Missing NEXT_PUBLIC_VAPI_API_KEY");
+  console.warn("Missing VAPI API_KEY");
 }
 
 export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
